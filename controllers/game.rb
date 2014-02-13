19ca8616a7eec16e05d2cdd6ba34c829df8e2223
Daniel Kimbel <dkkimbel@gmail.com>
2014-02-12 20:32:10 -0800

    Start to build out hand evaluation

--- controllers/game.rb
+++ controllers/game.rb
@@ -1,5 +1,9 @@
 class Game
 
+  HAND_NAMES = ['high card', 'pair', 'two pair', 'three of a kind',
+              'straight', 'flush', 'full house', 'four of a kind',
+              'straight flush']
+
   def initialize(text_line)
     @players = []
     @winner = nil
--- models/player.rb
+++ models/player.rb
@@ -3,6 +3,10 @@ class Player
   attr_accessor :rank
   attr_reader :name, :hand
 
+  VALUES_TO_NUMBERS = {'1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5,
+                       '6' => 6, '7' => 7, '8' => 8, '9' => 9, 'T' => 10,
+                       'J' => 11, 'Q' => 12, 'K' => 13, 'A' => 14}
+
   def initialize(args)
     @name = args[:name]
     @hand = args[:hand]
@@ -16,26 +20,20 @@ class Player
   private
 
   def determine_hand_value
-    # there has to be a much cleaner way to do this, which I will look for next; can't simply
-    # iterate through an array of functions as one could in Python, though
-    rank_found, rank = check_for_straight_flush
-    return rank if rank_found
-    rank_found, rank = check_for_four_of_a_kind
-    return rank if rank_found
-    rank_found, rank = check_for_full_house
-    return rank if rank_found
-    rank_found, rank = check_for_flush
-    return rank if rank_found
-    rank_found, rank = check_for_straight
-    return rank if rank_found
-    rank_found, rank = check_for_three_of_a_kind
-    return rank if rank_found
-    rank_found, rank = check_for_two_pair
-    return rank if rank_found
-    rank_found, rank = check_for_pair
-    return rank if rank_found
-    rank = get_high_card
-    return rank
+    is_flush = determine_whether_flush
+    is_straight = determine_whether_straight
+    num_same = determine_num_same
+  end
+
+  def determine_whether_flush
+    (self.hand[0].suit == self.hand[1].suit == self.hand[2].suit ==
+     self.hand[3].suit == self.hand[4].suit)
+  end
+
+  def determine_whether_straight
+  end
+
+  def determine_num_same
   end
 
 end
